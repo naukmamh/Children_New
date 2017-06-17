bf9d3b27f7579dd828b4d412126c81663e0969ce
natalya118 <nutblog88@gmail.com>
2017-06-17 19:02:38 +0300

    fixed bug with transactions

--- Children_Java-master/Children/src/main/java/com/children/model/House.java
+++ Children_Java-master/Children/src/main/java/com/children/model/House.java
@@ -1,8 +1,9 @@
 package com.children.model;
 
+import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
+import javax.persistence.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.transaction.Transactional;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
@@ -18,7 +20,8 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-public class House {
+@Transactional
+public class House implements Serializable{
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(nullable=false)
 	private int id;
