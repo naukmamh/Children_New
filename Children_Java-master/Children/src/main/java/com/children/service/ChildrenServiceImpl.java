c32d9e554ce2af6abfe6052f20f08ba241d9e461
oleksii <aar97@ukr.net>
2017-06-21 00:53:10 +0300

    Filters for all childrens
    Added filters for all childres;
    Unified to make as many filters as needed;
    Added //TODO in child dao for database queries;
    Added action to jsp form;
    Started javadoc;
    Modified child model with sex;
    Made work in child controller to handle queries with and without filters
    in one controller;

--- Children_Java-master/Children/src/main/java/com/children/service/ChildrenServiceImpl.java
+++ Children_Java-master/Children/src/main/java/com/children/service/ChildrenServiceImpl.java
@@ -1,6 +1,7 @@
 package com.children.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -52,6 +53,11 @@ public class ChildrenServiceImpl implements ChildrenService{
 	@Override
 	public List<Child> findAllChildrenByHouse(int houseId) {
 		return childDao.findAllChildrenByHouse(houseId);
+	}
+
+	@Override
+	public List<Child> findAllChildrenWithFilters(Map<String, String> filters) {
+		return childDao.findAllChildrenWithFilters(filters);
 	}
 
 }
